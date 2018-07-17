00fd4ad6685c5296c0f850231fd82b6fec5adcd6
ETATUNG\Zunzhe.Lin <lin19960921@gmail.com>
2018-07-17 11:17:00 +0800

    google map

--- app.js
+++ app.js
@@ -96,11 +96,15 @@ app.post('/messages', function (request, response) {
                 });*/
                 SendLinePayMessage(acct, results[idx].message.text, 'tstiisacompanyfortatung', reply_token, function (ret) {
                 });
+                initMap();
             } else if (results[idx].message.type == 'location') {
                 logger.info('緯度: ' + results[idx].message.latitude);
                 logger.info('經度: ' + results[idx].message.longitude);
                 /*104台北市中山區104台北市中山區中山北路三段13號
-                25.064258, 121.522554 */
+                25.064258, 121.522554 
+                台北市103大同區重慶北路二段174-2號
+                25.059610, 121.513416
+                */
             }
         }
     }
@@ -120,6 +124,82 @@ process.on('uncaughtException', function (err) {
     logger.error('uncaughtException occurred: ' + (err.stack ? err.stack : err));
 });
 
+/*********************************************************888 */
+function initMap() {
+    var bounds = new google.maps.LatLngBounds;
+    var markersArray = [];
+    var origin = {lat: 25.064258, lng: 121.522554};
+    var destination = {lat: 25.059610, lng: 121.513416};
+    var destinationIcon = 'https://chart.googleapis.com/chart?' +
+        'chst=d_map_pin_letter&chld=D|FF0000|000000';
+    var originIcon = 'https://chart.googleapis.com/chart?' +
+        'chst=d_map_pin_letter&chld=O|FFFF00|000000';
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 55.53, lng: 9.4},
+      zoom: 10
+    });
+    var geocoder = new google.maps.Geocoder;
+  
+    var service = new google.maps.DistanceMatrixService;
+    service.getDistanceMatrix({
+      origins: [origin],
+      destinations: [destination],
+      travelMode: 'DRIVING',
+      unitSystem: google.maps.UnitSystem.METRIC,
+      avoidHighways: false,
+      avoidTolls: false
+    }, function(response, status) {
+      if (status !== 'OK') {
+        alert('Error was: ' + status);
+      } else {
+        var originList = response.originAddresses;
+        var destinationList = response.destinationAddresses;
+        var outputDiv = document.getElementById('output');
+        outputDiv.innerHTML = '';
+        deleteMarkers(markersArray);
+  
+        var showGeocodedAddressOnMap = function(asDestination) {
+          var icon = asDestination ? destinationIcon : originIcon;
+          return function(results, status) {
+            if (status === 'OK') {
+              map.fitBounds(bounds.extend(results[0].geometry.location));
+              markersArray.push(new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+                icon: icon
+              }));
+            } else {
+              alert('Geocode was not successful due to: ' + status);
+            }
+          };
+        };
+  
+        for (var i = 0; i < originList.length; i++) {
+          var results = response.rows[i].elements;
+          geocoder.geocode({'address': originList[i]},
+              showGeocodedAddressOnMap(false));
+          for (var j = 0; j < results.length; j++) {
+            geocoder.geocode({'address': destinationList[j]},
+                showGeocodedAddressOnMap(true));
+            /*outputDiv.innerHTML += originList[i] + ' to ' + destinationList[j] +
+                ': ' + results[j].distance.text + ' in ' +
+                results[j].duration.text + '<br>';*/
+            console.log("---------------------------------------------------------" + originList[i] + ' to ' + destinationList[j] +
+            ': ' + results[j].distance.text + ' in ' +
+            results[j].duration.text);
+          }
+        }
+      }
+    });
+  }
+  
+  function deleteMarkers(markersArray) {
+    for (var i = 0; i < markersArray.length; i++) {
+      markersArray[i].setMap(null);
+    }
+    markersArray = [];
+  }
+/**********************************************************/
 // 傳送訊息給 LINE 使用者
 function SendLinePayMessage(userId, message, password, reply_token, callback) {
     var date = new Date();
