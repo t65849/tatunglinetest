b3023d4f2ddf7237799492e1d69c6fbb6c726b1a
ETATUNG\Andy.Wu <t658493213@gmail.com>
2018-08-03 15:26:24 +0800

    test flex

--- app.js
+++ app.js
@@ -1311,44 +1311,32 @@ function SendQuickReplies(userId, richmenumessage, password, reply_token, callba
     if (password == 'tstiisacompanyfortatung') {
         console.log('SendQuickReplies');
         //if (richmenumessage == 'the user has no richmenu') {
-            var data = {
-                'to': userId,
-                'messages': [
-                    {
-                        "type": "text", // ①
-                        "text": "Select your favorite food category or send me your location!",
-                        "quickReply": { // ②
-                            "items": [
+        var data = {
+            'to': userId,
+            'messages': [
+                {
+                    "type": "flex",
+                    "altText": "this is a flex message",
+                    "contents": {
+                        "type": "bubble",
+                        "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
                                 {
-                                    "type": "action", // ③
-                                    "imageUrl": "https://example.com/sushi.png",
-                                    "action": {
-                                        "type": "message",
-                                        "label": "Sushi",
-                                        "text": "Sushi"
-                                    }
+                                    "type": "text",
+                                    "text": "hello"
                                 },
                                 {
-                                    "type": "action",
-                                    "imageUrl": "https://example.com/tempura.png",
-                                    "action": {
-                                        "type": "message",
-                                        "label": "Tempura",
-                                        "text": "Tempura"
-                                    }
-                                },
-                                {
-                                    "type": "action", // ④
-                                    "action": {
-                                        "type": "location",
-                                        "label": "Send location"
-                                    }
+                                    "type": "text",
+                                    "text": "world"
                                 }
                             ]
                         }
                     }
-                ]
-            }; //end data
+                }
+            ]
+        }; //end data
         /*} else {
             var data = {
                 'to': userId,
