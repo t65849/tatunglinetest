cd1bc955ad8d94c891128b7651ab614e75605286
ETATUNG\Andy.Wu <t658493213@gmail.com>
2019-04-26 10:38:37 +0800

    加點

--- app.js
+++ app.js
@@ -248,7 +248,7 @@ app.post('/messages', function (request, response) {
                                                     }
                                                 }
                                             } else if(text.indexOf('@') != -1){
-                                                if(text.slice(0,1) != '@'){ //排除LINE ID前面的@
+                                                if(text.slice(0,1) != '@' && text.indexOf('.') != -1){ //排除LINE ID前面的@
                                                     for(var e=0; e<words.length;e++){
                                                         email = email+words[e].text;
                                                     }
