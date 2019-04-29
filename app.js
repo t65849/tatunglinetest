64efbea9b698b01ba6c4b1a598c312c551347c78
ETATUNG\Andy.Wu <t658493213@gmail.com>
2019-04-29 10:23:33 +0800

    fix bug

--- app.js
+++ app.js
@@ -284,10 +284,10 @@ app.post('/messages', function (request, response) {
                                         } else if((line_text.toLowerCase()).indexOf("tel") != -1 || line_text.indexOf("市話") != -1){
                                             var check_tel = line_text.toLowerCase();
                                             if(check_tel.indexOf("fax") != -1){ //當tel和fax同一行
-                                                //check_tel = check_tel.split("fax")[0];
-                                                console.log(check_tel.split("fax")[1]);
-                                                /*fax = check_tel.split("fax")[1];
-                                                fax = fax.replace(":", "");*/
+                                                var splitfax = check_tel.split("fax")[1];
+                                                check_tel = check_tel.split("fax")[0];
+                                                fax = splitfax;
+                                                fax = fax.replace(":", "");
                                             }
                                             check_tel = check_tel.replace("tel", "");
                                             check_tel = check_tel.replace("市話", "");
