343827a087ba4052ef9d4c78377086d9bb9a998d
ETATUNG\Andy.Wu <t658493213@gmail.com>
2019-04-30 09:34:56 +0800

    測試

--- app.js
+++ app.js
@@ -223,7 +223,7 @@ app.post('/messages', function (request, response) {
                             req(options, function (error, response, body) {
                                 if (error) throw new Error(error);
                                 var text_to_Json = JSON.parse(body);
-                                //console.log(body);
+                                console.log(body);
                                 var regions = text_to_Json.regions;
                                 //console.log(regions.length);
                                 //console.log(regions);
@@ -279,11 +279,13 @@ app.post('/messages', function (request, response) {
                                             }
                                         } else if(line_text.indexOf("公司")!= -1 || line_text.indexOf("銀行")!= -1 || line_text.indexOf("银行")!= -1 || line_text.indexOf("國際")!= -1 || line_text.indexOf("国际")!= -1 || line_text.indexOf("科技")!= -1 || line_text.indexOf("院")!= -1 || line_text.indexOf("矽谷")!= -1 || line_text.indexOf("集團")!= -1 || line_text.indexOf("集团")!= -1 || line_text.indexOf("大學")!= -1 || line_text.indexOf("大学")!= -1 || line_text.indexOf("聯盟")!= -1 || line_text.indexOf("联盟")!= -1 || line_text.indexOf("人壽")!= -1 || line_text.indexOf("人寿")!= -1 || (line_text.toLowerCase()).indexOf("inc.")!= -1 || (line_text.toLowerCase()).indexOf("co.")!= -1 || (line_text.toLowerCase()).indexOf("technology")!= -1){
                                             console.log('------------company');
+                                            console.log(line_text);
                                             var check_company0 = line_text.split("公司")[0];
                                             var check_company1 = line_text.split("公司")[1];
                                             check_company0 = check_company0+'公司';
                                             console.log('******'+check_company0);
                                             line_text = line_text+check_company1; //公司後面的字串丟回line_text
+                                            console.log(line_text);
                                             console.log('&&&&&&'+line_text);
                                             company = check_company0;
                                         }  else if (line_text.indexOf("路")!= -1 || line_text.indexOf("市")!= -1 || line_text.indexOf("室")!= -1 || line_text.indexOf("樓")!= -1 || line_text.indexOf("楼")!= -1){
