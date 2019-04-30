6f5db68bd437a215d51ef2bfefe11b3fee786e30
ETATUNG\Andy.Wu <t658493213@gmail.com>
2019-04-30 11:48:12 +0800

    test

--- app.js
+++ app.js
@@ -270,7 +270,7 @@ app.post('/messages', function (request, response) {
                                             //console.log(text);
                                             //console.log(all_text);
                                         }
-                                        var pattern = new RegExp(/^0(2|3|37|4|49|5|6|7|8|82|89|826|836)\d$/);/*{7,8}*/
+                                        var pattern = new RegExp(/^0(2|3|37|4|49|5|6|7|8|82|89|826|836)\d{7,8}$/);/* */
                                         line_text = line_text.replace('-','').replace('-','').replace('-','');
                                         console.log(line_text);
                                         if((line_text.indexOf('@') != -1)){ //email
